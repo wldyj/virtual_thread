37db88a1a83363442c3a86f1808e5e8221f3adf2
JIE <2416338031@qq.com>
2024-12-30 11:22:54 +0800

    feat: 创建虚拟线程项目
    - 新增 .gitattributes、.gitignore 文件，配置项目版本控制
    - 添加 application.yml 配置文件，设置 Spring 应用名称
    - 创建 build.gradle 文件，配置项目构建和依赖
    - 设置 gradle-wrapper.properties 和 gradlew脚本，初始化 Gradle 构建工具
    - 新增 VirtualThreadApplication 和 VirtualThreadApplicationTests 类，构建 Spring Boot 应用骨架

---
+++ src/main/java/com/jie/virtual_thread/VirtualThreadApplication.java
@@ -0,0 +1,18 @@
+package com.jie.virtual_thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 虚拟线程
+ * 
+ * @author jie
+ */
+@SpringBootApplication
+public class VirtualThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualThreadApplication.class, args);
+    }
+
+}
